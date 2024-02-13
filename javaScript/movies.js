794aa77a2bf5a8f750ea49a81a0c4cd418a8b5d4
Roman <romankurockin473@gmail.com>
2024-02-13 11:53:09 +0200

    practice2

--- javaScript/movies.js
+++ javaScript/movies.js
@@ -13,10 +13,31 @@ const personalMovieDB = {
 };
 
 //3 Задайте пользователю по два раза вопросы 
+
+
+
+
 const oneOfLastMovies = prompt("One of the last movies you watched?",""),
 	rate = prompt("How much did you rate it?"),
 	oneOfLastMovies2 = prompt("One of the last movies you watched?",""),
 	rate2 = prompt("How much did you rate it?");
+
+	console.log(oneOfLastMovies);
+//1&2
+	let validation = false;
+
+	while (validation === false){
+		oneOfLastMovies,
+		rate;
+		if ((oneOfLastMovies === "" || rate === "" || (oneOfLastMovies === "" && rate === "")) || oneOfLastMovies.length > 50){
+			oneOfLastMovies,
+			rate;
+		}
+		else{
+			validation = true;
+		}
+		
+	}
 
 
 personalMovieDB.movies[oneOfLastMovies] = rate; //записываю output в обьект
@@ -25,3 +46,4 @@ personalMovieDB.movies[oneOfLastMovies2] = rate2; //записываю output в
 //git add -A добавление всех новых файлов
 //git commit -a -m"комент"" 
 console.log(personalMovieDB);
+
