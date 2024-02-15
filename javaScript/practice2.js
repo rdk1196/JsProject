30e60c77415bc10f099016709a67919ba67b9764
Roman <romankurockin473@gmail.com>
2024-02-15 20:23:43 +0200

    refactoring

--- javaScript/practice2.js
+++ javaScript/practice2.js
@@ -8,15 +8,34 @@ const personalMovieDB = {
 	genres : [],
 	private : false
 };
-const oneOfLastMovies = prompt("One of the last movies you watched?",""),
-	rate = prompt("How much did you rate it?"),
-	oneOfLastMovies2 = prompt("One of the last movies you watched?",""),
-	rate2 = prompt("How much did you rate it?");
 
-personalMovieDB.movies[oneOfLastMovies] = rate; //записываю output в обьект
-personalMovieDB.movies[oneOfLastMovies2] = rate2; //записываю output в обьект
+//practice2 ex1
+first: for (let i = 0; i < 2; i++){
+    const oneOfLastMovies = prompt("One of the last movies you watched?",""),
+	rate = prompt("How much did you rate it?");
+    if (oneOfLastMovies.length === 0 || oneOfLastMovies.length > 50) continue first;
+    else{
+        personalMovieDB.movies[oneOfLastMovies] = rate;
+    }
+}
+
+if (personalMovieDB.count < 10){
+    console.log("Few films watched");
+}
+else if (personalMovieDB.count > 10 || personalMovieDB.count < 30){
+    console.log("You're a great viewer");
+}
+else if (personalMovieDB.count > 30){
+    console.log("you a movie buff");
+}
+else{
+    console.log("Error");
+}
+
+
 
 // //git add -A добавление всех новых файлов
 // //git commit -a -m"комент"" 
 console.log(personalMovieDB);
-//  console.log("Hello");
+
+
